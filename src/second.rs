26335686211ae96a9c940203629ba2a443b0dd4a
chinmaydd <chinmay1dd@gmail.com>
2016-12-04 12:57:41 +0530

    Solves day 2

--- src/second.rs
+++ src/second.rs
@@ -14,16 +14,31 @@ fn read_into_string(filepath: &str) -> String {
     input
 }
 
+// Contains the main function logic.
+// We again use pattern matching for simplifying the problem solving by including border cases.
+// This can be done in a much better way maybe by using a static 'keypad' for accesses.
+// Given the current key and the sequence of operations to be performed, it returns the final key
+// which is pressed.
 fn next_key(key_seq: &str, init_key: i32) -> i32 {
     let mut curr_key = init_key;
     
-    for direction in keq_seq.chars() {
+    for direction in key_seq.chars() {
+        match (direction, curr_key) {
+            ('U', 1) |  ('L', 1) | ('L', 4) | ('L', 7) | ('D', 7) | ('U', 2) | ('D', 8) | ('U', 3) | ('R', 3) | ('R', 6) | ('R', 9) | ('D', 9) => {},
+            ('U', val) => {curr_key = val - 3},
+            ('D', val) => {curr_key = val + 3},
+            ('L', val) => {curr_key = val - 1},
+            ('R', val) => {curr_key = val + 1},
+            (_, _) => panic!("Something is wrong!")
+        }
     }
-    
-    // dummy value
+
     curr_key
 }
-
+    
+// Function which takes in a long string of operations, seperated by newline characters and applies
+// the next_key() function on each of them.
+// It returns the final code string as asked in the problem.
 fn find_code(input: String) -> String {
     let mut final_code = String::from("");
     
