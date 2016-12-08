205a731d7a7c0e4a427d0965bdf66a6f2220c864
chinmaydd <chinmay1dd@gmail.com>
2016-12-08 14:03:34 +0530

    Solves day 5.2

--- src/d5.rs
+++ src/d5.rs
@@ -2,6 +2,7 @@ extern crate crypto;
 
 use self::crypto::digest::Digest;
 use self::crypto::md5::Md5;
+use std::collections::HashMap;
 use std;
 
 pub fn run() {
@@ -9,6 +10,10 @@ pub fn run() {
 
     let key = "wtnhxymk".as_bytes();
     let mut count = 0;
+    let answer: String = String::from("");
+    let mut first_char: char;
+    let mut second_char: char;
+    let mut solved_map = HashMap::new();
 
     for i in 0..std::u64::MAX {
         hasher.input(key);
@@ -17,18 +22,19 @@ pub fn run() {
         let mut output = [0;16];
         hasher.result(&mut output);
 
-        let mut answer: String = String::from("");
 
         let first_five = output[0] as i32 + output[1] as i32 + (output[2] >> 4) as i32;
         if first_five == 0 {
-            answer.push(hasher.result_str()
-                              .chars()
-                              .nth(5)
-                              .unwrap());
+            first_char = hasher.result_str().chars().nth(5).unwrap();
+            second_char = hasher.result_str().chars().nth(6).unwrap();
 
-            count += 1;
-            if count == 8 {
-                println!("[5.1]: {}", answer);
+            if !first_char.is_alphabetic() && !solved_map.contains_key(&first_char) {
+                println!("{} {}", first_char, second_char);
+                solved_map.insert(first_char, second_char);
+                count += 1;
+            }
+
+            if count == 9 {
                 break;
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,9 @@ mod d4;
 mod d5;
 
 fn main() {
-    d1::run();
-    d2::run();
-    d3::run();
-    d4::run();
+    //d1::run();
+    //d2::run();
+    //d3::run();
+    //d4::run();
     d5::run();
 }
